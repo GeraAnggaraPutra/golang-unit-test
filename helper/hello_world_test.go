f30f00d3f4efc9b5b603ec1ae3e209bfff76391a
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-06 17:49:48 +0700

    assertion dengan testify

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,7 +3,28 @@ package helper
 import (
 	"fmt"
 	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+// asertt => Fail
+// require => FailNow
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Gera")
+	require.Equal(t, "Hell, Gera", result, "Result must be Hello, Gera") // harus sama 
+	fmt.Println("TestHelloWorld with Require, Done")
+}
+func TestHelloWorldAssert1(t *testing.T) {
+	result := HelloWorld("Gera")
+	assert.Equal(t, "Hello, Gera", result, "Result must be Hello, Gera") // harus sama 
+	fmt.Println("TestHelloWorld with Assert, Done")
+}
+func TestHelloWorldAssert2(t *testing.T) {
+	result := HelloWorld("Gera")
+	assert.NotEqual(t, "Hello, Anggara", result, "Result must be Hello, Gera") // harus beda
+	fmt.Println("TestHelloWorld with Assert, Done")
+}
 
 func TestHelloWorldGera(t *testing.T) {
 	result := HelloWorld("Gera")
